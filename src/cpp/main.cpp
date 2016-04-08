2ad58fdadc99d7bf04ef9fe1b3cb194fef552ab9
Manasses Ferreira Neto <manassesferreira@vinho.crc.dcc.ufmg.br>
2016-04-08 14:13:33 -0300

    scripts para compilar e executar

---
+++ src/cpp/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+main(int argc, char **argv) {
+    for(int i = 0; i < argc; ++i){
+        cout << "argv[" << i << "]: " << argv[i] << endl;
+    }
+    while(1){
+    }
+}
---
+++ src/java/executar.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+in=$1
+out=$2
+java Main $in $out
